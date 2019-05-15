766ffa0ce2bbde5db56ee5d25606933aebae33e0
aswinwibisurya <a.wibisurya@yahoo.co.id>
2019-05-15 19:48:07 +0700

    Fix port

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 const publicPath = path.join(__dirname, 'public')
 app.use(express.static(publicPath))
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`server running at ${port}`);
